23ab11d0f80487f241349613adf135a5d1ffcf0d
AbSamrah <syria.aa.2004@gmail.com>
2025-02-12 19:51:06 +0300

    Adding Domain

---
+++ BookStore/BookStore.API/models/Domain/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.API.models.Domain
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string AuthorName { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+        public double PriceInSYR { get; set; }
+    }
+}
---
+++ BookStore/BookStore.API/models/Domain/Purchase.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStore.API.models.Domain
+{
+    public class Purchase
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+        public double GetTotalPrice()
+        {
+            double totalPrice = 0;
+            foreach (var book in Books)
+            {
+                totalPrice += book.PriceInSYR;
+            }
+            return totalPrice;
+        }
+    }
+}
