a6e30ec22cd89bafc6fdbc913c555f26e49e7e9b
AbSamrah <syria.aa.2004@gmail.com>
2025-02-12 20:06:19 +0300

    Add DbContext

---
+++ BookStore/BookStore.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookStore.API.models.Domain;
+
+namespace BookStore.API.Data
+{
+    public class BookStoreDbContext: DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
