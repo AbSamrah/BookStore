5767edeb69ee0aa75d93bd4b561b7ee8a5814066
AbSamrah <syria.aa.2004@gmail.com>
2025-02-13 04:16:03 +0300

    solving error

---
+++ BookStore/BookStore.API/Profiles/PurchasesProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace BookStore.API.Profiles
+{
+    public class PurchasesProfile: Profile
+    {
+        public PurchasesProfile()
+        {
+            CreateMap<models.Domain.Purchase, models.DTO.Purchase>().ReverseMap();
+        }
+    }
+}
