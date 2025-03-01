61a962726d570321161b2a0d7eecc34086cf953b
AbSamrah <syria.aa.2004@gmail.com>
2025-03-01 23:44:19 +0300

    ..

---
+++ BookStore/BookStore.UI/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace BookStore.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory )
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7167/api/Auth/login"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+
+
+            return View();
+        }
+    }
+}
